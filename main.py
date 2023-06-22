cdccaa9c5ac090dbd3fac94a6ad011f409328601
arnaskap <arnas.kapustinskas@gmail.com>
2023-06-22 21:49:08 +0100

    wip

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello World')
+import requests
+
+url = 'https://service.berlin.de/standorte/buergeraemter/'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print(response.content)
+else:
+    print(f'Request failed with code {response.status_code}')
