6365e07846be03e1fa723cb79fc8ac55e7717765
arnaskap <arnas.kapustinskas@gmail.com>
2023-06-23 07:24:01 +0100

    unclean version that retrieves correct html

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+from bs4 import BeautifulSoup
 import requests
 
-url = 'https://service.berlin.de/standorte/buergeraemter/'
+WRITE_HTML = True
 
-response = requests.get(url)
+appt_calendar_url = 'https://service.berlin.de/dienstleistung/120686/'
 
-if response.status_code == 200:
-    print(response.content)
-else:
-    print(f'Request failed with code {response.status_code}')
+session = requests.Session()
+response = session.get(appt_calendar_url, allow_redirects=True)
+
+if response.status_code != 200:
+    raise Exception(f'Request failed with code {response.status_code}')
+
+response = session.get('https://service.berlin.de/terminvereinbarung/termin/all/120686/', allow_redirects=True)
+
+if response.status_code != 200:
+    raise Exception(f'Request failed with code {response.status_code}')
+
+appt_calendar_soup = BeautifulSoup(response.content, 'html.parser')
+print(appt_calendar_soup.prettify())
+
+if WRITE_HTML:
+    file_path = 'output/startpage.html'
+    file = open(file_path, 'w')
+    file.write(appt_calendar_soup.prettify())
